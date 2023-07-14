6a1eecfe5924e9a6e41a386bc626c6a5c1b9c0f1
ravena <ravena.carneiro@gmail.com>
2023-07-14 14:55:13 -0300

    Homolog Ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -28,7 +28,7 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection project){
         id = project.getId();
         title = project.getTitle();
-        year = project.getYear();
+        year = project.getGameYear();
         imgUrl = project.getImgUrl();
         shortDescription = project.getShortDescription();
     }
